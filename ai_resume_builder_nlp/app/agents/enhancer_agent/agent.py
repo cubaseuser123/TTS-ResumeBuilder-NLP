66ee1df4a5a11e92effb577c828f57b18f69abe2
unknown <aaryantajanpure@gmail.com>
2026-01-07 15:47:36 +0530

    enhancement agent is now functional

--- ai_resume_builder_nlp/app/agents/enhancer_agent/agent.py
+++ ai_resume_builder_nlp/app/agents/enhancer_agent/agent.py
@@ -2,11 +2,20 @@ import sys
 from os.path import dirname, join, abspath
 sys.path.append(abspath(join(dirname(__file__),"..","..")))
 from utils.file_loader import load_instructions_file
-from google.adk.agents import Agent
+from google.adk.agents import LlmAgent
 from nlp.enhancers.text_enhancer import enhance_resume_content
 
-enhancement_agent = Agent(
-    name="enhancement_agent",
+def pre_enhance(state: dict) -> dict:
+    return{
+        "pre_enhanced_content": enhance_resume_content(state)
+    }
+
+
+enhancement_agent = LlmAgent(
+    name="enhancement_agent", 
+    model='gemini-2.5-flash',
+    instructions=load_instructions_file("agents/enhancer_agent/instructions.txt"),
     description=load_instructions_file("agents/enhancer_agent/description.txt"),
-    # tools=[enhance_resume_content]
+    temperature=0.25,
+    tools=[pre_enhance]
 )
